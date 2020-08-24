a82643fabcca8eb84bf74a5b20a952a6dadf1179
akazad1235 <akazad914@gmail.com>
2020-08-25 00:49:04 +0600

    ema-john logo and header section added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css'
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logo} alt='logo'/>
+            <nav>
+                <a href="/shop">Shop</a>
+                <a href="/orderReview">Order Review</a>
+                <a href="/inventory">Manage Inventory Here</a>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import fakeData from '../../fakeData';
+
+const Shop = () => {
+
+    console.log(fakeData);
+    
+        
+
+        
+    return (
+        <div>
+            <h1>this si show</h1>
+        </div>
+    );
+};
+
+export default Shop;
